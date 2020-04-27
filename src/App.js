2f68b8542fd66308a3c0b718a9ad0f09a1b710b4
Vinay Desai <vinaydesaicoding@gmail.com>
2020-04-27 21:16:53 +0100

    Uploaded upto and including task 8 of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import mementoCover from './images/memento.jpg';
+import americanPsychoCover from './images/americanPsycho.jpg';
+import theWolfOfWallStreetCover from './images/theWolfOfWallStreet.jpg';
+import inceptionCover from './images/inception.jpg';
+import boratCover from './images/borat.jpg';
+import thisIsSpinalTapCover from './images/thisIsSpinalTap.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Welcome to Kodflix</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Welcome to Kodflix</h1>  
+        <div className='container'> 
+          <div className='item'><img src={mementoCover}  alt='memento' />
+            <div className='overlay'><h2>Memento</h2></div>
+          </div>
+          <div className='item'><img src={americanPsychoCover} alt='americanPsycho' />
+          <div className='overlay'><h2>American Psycho</h2></div>
+          </div>
+          <div className='item'><img src={theWolfOfWallStreetCover} alt='theWolfOfWallStreet' />
+            <div className='overlay'><h2>The Wolf of Wall Street</h2></div>
+          </div>
+        </div> 
+        <div className='container'>
+          <div className='item'><img src={inceptionCover} alt='inception' />
+            <div className='overlay'><h2>Inception</h2></div>
+          </div>
+          <div className='item'><img src={boratCover} alt='borat' />
+            <div className='overlay'><h2>Borat</h2></div>
+          </div>
+          <div className='item'><img src={thisIsSpinalTapCover} alt='thisIsSpinalTap' />
+            <div className='overlay'><h2>This is Spinal Tap</h2></div>
+          </div>
+        </div>
+      </div>
+    </div>   
   );
 }
 
